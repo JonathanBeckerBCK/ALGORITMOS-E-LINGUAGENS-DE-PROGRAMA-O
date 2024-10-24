a779d5d95dfe0bc1cd20ae85ab9b56a2286c505a
JonathanBeckerBCK <jonathanbe.ifpr2024@gmail.com>
2024-10-24 08:20:12 -0300

    1.ex

---
+++ 1.ex
@@ -0,0 +1,18 @@
+programa
+{
+	
+	funcao inicio()
+	{
+	   escreva("1. Escreva um algoritmo que dado um vetor, já inicializado, de 5 números inteiros escolhidos por você, exiba cada elemento desse vetor, um abaixo do outro.\n")
+					   // 0,1,2,3,4
+	     inteiro vetor[5]={10,20,30,40,50}	
+
+		   para(inteiro i=0; i < 5; i++)
+		   {
+
+			 escreva(vetor[i] ,"\n")
+					
+		   }
+	
+	}
+}
