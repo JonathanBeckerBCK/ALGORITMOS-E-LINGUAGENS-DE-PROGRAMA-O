eabdd48d0f3a0acd6d9c7b08a2f85d3cd2aa47a2
JonathanBeckerBCK <jonathanbe.ifpr2024@gmail.com>
2024-10-24 08:29:50 -0300

    2.ex

--- 2.ex
+++ 2.ex
@@ -1,1 +1,18 @@
+programa
+{
+	
+	funcao inicio()
+	{
+	   escreva("2. Escreva um algoritmo que exiba o mesmo vetor do exercício anterior em ordem inversa.\n")
+					   // 0,1,2,3,4
+	     inteiro vetor[5]={10,20,30,40,50}	
 
+		   para(inteiro i=4; 0 <= i  ; i--)
+		   {
+
+			 escreva(vetor[i] ,"\n")
+					
+		   }
+	
+	}
+}
