8597e4ae6cb3a538599d1dabe7bbe39a08934e64
JonathanBeckerBCK <jonathanbe.ifpr2024@gmail.com>
2024-10-24 09:04:09 -0300

    3.ex

--- 3.ex
+++ 3.ex
@@ -1,1 +1,23 @@
+programa
+{
+	
+	funcao inicio()
+	{
+	   escreva("3. Escreva um algoritmo que leia 4 notas, mostre as notas e a média na tela. As notas devem ser armazenadas em um vetor de 4 posições. A leitura das notas deve ser realizada dentro de um laço de repetição.\n")
+					   // 0,1,2,3,4
+	     cadeia vetor[4]={"a","b","c","d"}	
 
+		   leia(vetor[0])
+		   escreva(vetor[0])
+
+		   leia(vetor[1])
+		   escreva(vetor[0])
+
+		   leia(vetor[0])
+		   escreva(vetor[0])
+
+		   leia(vetor[0])
+		   escreva(vetor[0])
+	
+	}
+}
